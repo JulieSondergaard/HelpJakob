3e1a97c80b78849fb11721eaaeedb710723c7360
JulieSøndergaard-Hilleke <jsh@itklyngen.dk>
2022-05-12 09:51:55 +0200

    Add project files.

---
+++ HelpJakob/Converter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpJakob
+{
+    /* This class can covert plaintext to HTML */
+    class Converter
+    {
+
+        public string ConvertBodyToHTML(string plainText)
+        {
+            return "" + plainText + "";
+        }
+    }
+}
---
+++ HelpJakob/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelpJakob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message msg = new Message("jules", "james", "hello world", "some subject", "bill");
+
+            MessageSender msgSender = new MessageSender();
+
+            msgSender.sendMessage(MessageCarrier.Smtp, msg, true);
+        }
+    }
+
+
+}
